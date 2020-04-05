b0ed6a34659cda3e272356cd4b4e3afe864b820e
Arram <arram@tutanota.com>
2020-04-05 14:21:20 +0200

    Styling ended, files divided

---
+++ components/Decide.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="decision">
+    <p class="question">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+    <div class="card">
+      <p></p>
+      <img src="~assets/img/florka.jpg" alt="florka" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Decide'
+};
+</script>
